08cbacc2b4e202b19731d815e358c0c00a564ce6
Khalil's MAC <khalilobedjaoui@gmail.com>
2023-02-28 16:42:16 +0100

    Class created

---
+++ untitled/src/Class/Global.java
@@ -0,0 +1,11 @@
+package Class;
+
+public class Global {
+
+    private int newConfirmed;
+    private int totalConfirmed;
+    private int newDeaths;
+    private int totalDeaths;
+    private int newRecovered;
+    private int totalRecovered;
+}
---
+++ untitled/src/Class/Summary.java
@@ -0,0 +1,14 @@
+package Class;
+
+import lombok.Getter;
+
+import java.util.Date;
+
+@Getter
+public class Summary {
+
+    private String message;
+    private Countries countries;
+    private Global global;
+    private Date date;
+}
