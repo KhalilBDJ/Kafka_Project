5ce78bca2f67e10e2963a14f02e82350ac5e0fba
Khalil <khalil.bedjaoui@lojelis.com>
2023-03-08 21:42:20 +0100

    First producer done

--- KafkaProducer1/src/main/java/org/example/CovidDataProducer.java
+++ KafkaProducer1/src/main/java/org/example/CovidDataProducer.java
@@ -6,13 +6,18 @@ import org.apache.kafka.clients.producer.ProducerConfig;
 import org.apache.kafka.clients.producer.ProducerRecord;
 import org.apache.kafka.clients.producer.KafkaProducer;
 import org.apache.kafka.common.serialization.StringSerializer;
+
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.Properties;
 
 public class CovidDataProducer {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         // configure producer properties
         Properties props = new Properties();
         props.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
@@ -20,19 +25,29 @@ public class CovidDataProducer {
         props.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
 
         // create kafka producer
-        Producer<String, String> producer = new KafkaProducer<String, String>(props);
 
-        // fetch covid data from API
-        String url = "https://api.covid19api.com/summary";
-        ObjectMapper objectMapper = new ObjectMapper();
-        //CovidData covidData = objectMapper.readValue(new URL(url), CovidData.class);
-        //String json = objectMapper.writeValueAsString(covidData);
 
-        // send data to kafka
-        ProducerRecord<String, String> record = new ProducerRecord<>("testTopic", "CECI EST UN TEST");
+        URL url = new URL("https://api.covid19api.com/summary");
+        HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+        conn.setRequestMethod("GET");
+
+        BufferedReader in = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+        String inputLine;
+        StringBuilder response = new StringBuilder();
+
+        while ((inputLine = in.readLine()) != null) {
+            response.append(inputLine);
+        }
+
+        in.close();
+        String jsonString = response.toString();
+        String topicName = "testTopic";
+
+        KafkaProducer<String, String> producer = new KafkaProducer<String, String>(props);
+
+        ProducerRecord<String, String> record = new ProducerRecord<String, String>(topicName, jsonString);
         producer.send(record);
 
-        // close producer
         producer.close();
     }
 }
