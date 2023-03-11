da2d8dcb361004f585882e2e636c325ff2ba5af7
Khalil <khalil.bedjaoui@lojelis.com>
2023-03-11 14:36:27 +0100

    Deletion added

--- KafkaProducer1/src/main/java/org/example/CovidDataConsumer.java
+++ KafkaProducer1/src/main/java/org/example/CovidDataConsumer.java
@@ -1,15 +1,17 @@
 package org.example;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.PreparedStatement;
-import java.sql.SQLException;
+import java.sql.*;
 import java.time.Duration;
 import java.util.Collections;
 import java.util.Properties;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.apache.kafka.clients.consumer.*;
 import org.apache.kafka.common.serialization.StringDeserializer;
+import org.example.Classes.Countries;
+import org.example.Classes.Global;
 import org.json.JSONException;
 import org.json.JSONObject;
 
@@ -20,13 +22,16 @@ public class CovidDataConsumer {
     private static final String DB_USER = "postgres";
     private static final String DB_PASSWORD = "1797";
 
-    public static void main(String[] args) throws JSONException {
+    public static void main(String[] args) throws JSONException, SQLException, JsonProcessingException {
 
         // Configurer les propriétés du consumer
         Properties props = new Properties();
         props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
+        props.put(ConsumerConfig.GROUP_ID_CONFIG, "ConsumerGroup");
         props.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
         props.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
+        props.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest");
+
 
         // Créer le consumer
         Consumer<String, String> consumer = new KafkaConsumer<>(props);
@@ -34,29 +39,55 @@ public class CovidDataConsumer {
         // Abonner le consumer au topic
         consumer.subscribe(Collections.singleton(TOPIC_NAME));
 
-        // Boucle infinie de consommation des messages
+        String url = "jdbc:postgresql://localhost:5432/kafkadb";
+        String user = "postgres";
+        String password = "1797";
+        Connection conn = DriverManager.getConnection(url, user, password);
+
+        ObjectMapper objectMapper = new ObjectMapper();
+
+        // Lecture des messages
         while (true) {
             ConsumerRecords<String, String> records = consumer.poll(Duration.ofMillis(100));
-
             for (ConsumerRecord<String, String> record : records) {
-                String jsonString = record.value();
-                JSONObject jsonObject = new JSONObject(jsonString);
+                // Convertir le message JSON en objet Java
+                String message = record.value();
+                JsonNode jsonNode = objectMapper.readTree(message);
+                String globalString = jsonNode.get("Global").toString();
+                Global global = objectMapper.readValue(globalString, Global.class);
+                JsonNode countriesNode = jsonNode.get("Countries");
+                Countries[] countries = objectMapper.readValue(countriesNode.toString(), Countries[].class);
 
-                // Récupérer les données de l'objet JSON et les stocker dans la base de données
-                String name = jsonObject.getString("name");
-                int age = jsonObject.getInt("age");
-                String city = jsonObject.getString("city");
+                // Insérer les données globales dans la table "global_data" de la base de données PostgreSQL
+                PreparedStatement statementGlobal = conn.prepareStatement("INSERT INTO global_data (new_confirmed, total_confirmed, new_deaths, total_deaths, new_recovered, total_recovered) VALUES (?, ?, ?, ?, ?, ?)");
+                statementGlobal.setInt(1, global.getNewConfirmed());
+                statementGlobal.setInt(2, global.getTotalConfirmed());
+                statementGlobal.setInt(3, global.getNewDeaths());
+                statementGlobal.setInt(4, global.getTotalDeaths());
+                statementGlobal.setInt(5, global.getNewRecovered());
+                statementGlobal.setInt(6, global.getTotalRecovered());
+                statementGlobal.executeUpdate();
 
-                try (Connection conn = DriverManager.getConnection(DB_URL, DB_USER, DB_PASSWORD);
-                     PreparedStatement pstmt = conn.prepareStatement("INSERT INTO users (name, age, city) VALUES (?, ?, ?)")) {
-                    pstmt.setString(1, name);
-                    pstmt.setInt(2, age);
-                    pstmt.setString(3, city);
-                    pstmt.executeUpdate();
-                } catch (SQLException e) {
-                    e.printStackTrace();
+                // Insérer les données de chaque pays dans la table "country_data" de la base de données PostgreSQL
+                for (Countries country : countries) {
+                    // Insérer les données du pays dans la table "country_data" de la base de données PostgreSQL
+                    PreparedStatement statementCountry = conn.prepareStatement("INSERT INTO country_data (country_code, country_name, new_confirmed, total_confirmed, new_deaths, total_deaths, new_recovered, total_recovered, date) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)");
+                    statementCountry.setString(1, country.getCountryCode());
+
+        // Boucle infinie de consommation des messages
+                    statementCountry.setString(2, country.getCountry());
+                    statementCountry.setInt(3, country.getNewConfirmed());
+                    statementCountry.setInt(4, country.getTotalConfirmed());
+                    statementCountry.setInt(5, country.getNewDeaths());
+                    statementCountry.setInt(6, country.getTotalDeaths());
+                    statementCountry.setInt(7, country.getNewRecovered());
+                    statementCountry.setInt(8, country.getTotalRecovered());
+                    statementCountry.setTimestamp(9, Timestamp.valueOf(String.valueOf(country.getDatemaj().getDay())));
+                    statementCountry.executeUpdate();
                 }
+                conn.commit();
             }
         }
     }
 }
+
--- KafkaProducer1/src/main/java/org/example/CovidDataProducer.java
+++ KafkaProducer1/src/main/java/org/example/CovidDataProducer.java
@@ -1,6 +1,7 @@
 package org.example;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import org.apache.kafka.clients.admin.*;
 import org.apache.kafka.clients.producer.Producer;
 import org.apache.kafka.clients.producer.ProducerConfig;
 import org.apache.kafka.clients.producer.ProducerRecord;
@@ -13,12 +14,22 @@ import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
-import java.util.Properties;
+import java.util.*;
+import java.util.concurrent.ExecutionException;
 import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.TimeUnit;
 
+import org.apache.kafka.common.TopicPartition;
+import org.apache.kafka.common.serialization.StringDeserializer;
+
+import java.time.Duration;
+import java.util.Properties;
+
 public class CovidDataProducer {
+
+
+    private static final String KAFKA_SERVERS = "localhost:9092"; // adresse du serveur kafka
 
     public static void main(String[] args) throws IOException {
         CovidDataProducer producer = new CovidDataProducer();
@@ -44,13 +55,26 @@ public class CovidDataProducer {
     private void SendCovidData() throws IOException {
         // configure producer properties
         Properties props = new Properties();
-        props.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
+        props.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, KAFKA_SERVERS);
         props.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
         props.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
 
+        AdminClient adminClient = AdminClient.create(props);
+
+        TopicPartition topicPartition = new TopicPartition("Topic1", 0);
+        Map<TopicPartition, RecordsToDelete> recordsToDelete = new HashMap<>();
+        recordsToDelete.put(topicPartition, RecordsToDelete.beforeOffset(1L));
+
+        DeleteRecordsResult deleteResult = adminClient.deleteRecords(recordsToDelete);
+        try {
+            deleteResult.all().get();
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        } catch (ExecutionException e) {
+            throw new RuntimeException(e);
+        }
+
         // create kafka producer
-
-
         URL url = new URL("https://api.covid19api.com/summary");
         HttpURLConnection conn = (HttpURLConnection) url.openConnection();
         conn.setRequestMethod("GET");
@@ -65,8 +89,8 @@ public class CovidDataProducer {
 
         in.close();
         String jsonString = response.toString();
-        String topicName = "testTopic";
-
+        String topicName = "Topic1";
+        //deleteTopicData("testTopic", 0, 1000000, adminClient);
         KafkaProducer<String, String> producer = new KafkaProducer<>(props);
 
         ProducerRecord<String, String> record = new ProducerRecord<>(topicName, jsonString);
@@ -74,4 +98,12 @@ public class CovidDataProducer {
 
         producer.close();
     }
+
+    public void deleteTopicData(String topicName, int partitionIndex, int beforeIndex, AdminClient kafkaAdminClient) {
+
+    }
+
+
+
+
 }
