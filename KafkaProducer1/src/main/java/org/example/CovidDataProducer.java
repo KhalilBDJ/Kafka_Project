2d2f6d9e74164fd325580c3dbf5d01def8cfe7a0
Khalil <khalil.bedjaoui@lojelis.com>
2023-03-15 12:04:10 +0100

    First migration done

--- KafkaProducer1/src/main/java/org/example/CovidDataProducer.java
+++ KafkaProducer1/src/main/java/org/example/CovidDataProducer.java
@@ -1,69 +1,64 @@
 package org.example;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
-import org.apache.kafka.clients.admin.*;
-import org.apache.kafka.clients.producer.Producer;
+import org.apache.kafka.clients.admin.AdminClient;
+import org.apache.kafka.clients.admin.AdminClientConfig;
+import org.apache.kafka.clients.admin.NewTopic;
 import org.apache.kafka.clients.producer.ProducerConfig;
 import org.apache.kafka.clients.producer.ProducerRecord;
-import org.apache.kafka.clients.producer.KafkaProducer;
 import org.apache.kafka.common.serialization.StringSerializer;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.kafka.annotation.EnableKafka;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.kafka.core.KafkaAdmin;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.support.serializer.JsonSerializer;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.beans.factory.support.BeanDefinitionBuilder;
 
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
-import java.net.MalformedURLException;
 import java.net.URL;
-import java.util.*;
-import java.util.concurrent.ExecutionException;
-import java.util.concurrent.Executors;
-import java.util.concurrent.ScheduledExecutorService;
-import java.util.concurrent.TimeUnit;
-
-import org.apache.kafka.common.TopicPartition;
-import org.apache.kafka.common.serialization.StringDeserializer;
-
-import java.time.Duration;
+import java.util.Collections;
 import java.util.Properties;
+import java.util.concurrent.ExecutionException;
 
+@SpringBootApplication
+@EnableKafka
+@EnableScheduling
 public class CovidDataProducer {
-
 
     private static final String KAFKA_SERVERS = "localhost:9092"; // adresse du serveur kafka
     private static final String TOPIC_NAME = "Topic1";
 
-    public static void main(String[] args) throws IOException {
-        CovidDataProducer producer = new CovidDataProducer();
-        producer.start();
+    @Autowired
+    private KafkaTemplate<String, Object> kafkaTemplate;
+
+    public static void main(String[] args) throws ExecutionException, InterruptedException {
+        ConfigurableApplicationContext context = SpringApplication.run(CovidDataProducer.class, args);
+
+        // Créer un bean KafkaAdmin pour pouvoir créer des topics
+        KafkaAdmin admin = (KafkaAdmin) context.getBean("kafkaAdmin");
+        AdminClient client = AdminClient.create(admin.getConfigurationProperties());
+
+        // Vérifier si le topic existe
+        if (!client.listTopics().names().get().contains(TOPIC_NAME)) {
+            // Créer le topic avec une partition et un replication factor de 1
+            NewTopic topic = new NewTopic(TOPIC_NAME, 1, (short) 1);
+            client.createTopics(Collections.singletonList(topic));
+        }
     }
 
-    private final ScheduledExecutorService scheduler = Executors.newSingleThreadScheduledExecutor();
-
-    public void start() {
-        scheduler.scheduleAtFixedRate(() -> {
-            try {
-                SendCovidData();
-            } catch (Exception e) {
-                System.err.println("Failed to send COVID data to Kafka: " + e.getMessage());
-            }
-        }, 0, 30, TimeUnit.MINUTES);
-    }
-
-    public void stop() {
-        scheduler.shutdown();
-    }
-
-    private void SendCovidData() throws IOException {
-        // configure producer properties
-        Properties props = new Properties();
-        props.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, KAFKA_SERVERS);
-        props.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
-        props.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
-
-        AdminClient adminClient = AdminClient.create(props);
-
-
-        // create kafka producer
+    // Envoyer les données COVID toutes les 30 minutes
+    @Scheduled(fixedDelay = 30 * 60 * 1000)
+    public void sendCovidData() throws IOException {
         URL url = new URL("https://api.covid19api.com/summary");
         HttpURLConnection conn = (HttpURLConnection) url.openConnection();
         conn.setRequestMethod("GET");
@@ -79,17 +74,19 @@ public class CovidDataProducer {
         in.close();
         String jsonString = response.toString();
         String topicName = TOPIC_NAME;
-        KafkaProducer<String, String> producer = new KafkaProducer<>(props);
 
-        ProducerRecord<String, String> record = new ProducerRecord<>(topicName, jsonString);
-        producer.send(record);
-
-        producer.close();
+        // Envoyer le message
+        ProducerRecord<String, Object> record = new ProducerRecord<>(topicName, jsonString);
+        kafkaTemplate.send(record);
     }
 
-
-
-
-
-
+    // Définir les propriétés du producteur
+    @Bean
+    public Properties producerConfig() {
+        Properties props = new Properties();
+        props.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, KAFKA_SERVERS);
+        props.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
+        props.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, JsonSerializer.class);
+        return props;
+    }
 }
--- KafkaProducer1/src/main/java/org/example/Main.java
+++ KafkaProducer1/src/main/java/org/example/Main.java
@@ -1,6 +1,7 @@
 package org.example;
 
 import java.io.IOException;
+import java.util.concurrent.ExecutionException;
 
 public class Main {
 
@@ -9,7 +10,7 @@ public class Main {
     private static CovidDataConsumer covidDataConsumer;
     private static CovidDataProducer covidDataProducer;
     private static APIProducerAndConsumer apiProducerAndConsumer;
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws IOException, ExecutionException, InterruptedException {
         covidDataConsumer.main(args);
         covidDataProducer.main(args);
         commandProducer.main(args);
